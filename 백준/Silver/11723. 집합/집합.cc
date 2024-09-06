392ab78c6cd283e9195dea6a0a8b4b8beed6d84d
Seoyoung Kim <ksysnake@hanyang.ac.kr>
2024-09-06 10:58:55 +0900

    [Silver V] Title: 집합, Time: 564 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.cc
+++ 백준/Silver/11723. 집합/집합.cc
@@ -1,10 +1,9 @@
 // 2024.09.06
-// BOJ_11723_집합
+// BOJ_11723_집합_비트마스킹풀이
 #include <bits/stdc++.h>
 using namespace std;
-int m, x;
+int m, n, x;
 string s;
-map<int, int> mp;
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
@@ -12,29 +11,20 @@ int main() {
 	for (int i = 0; i < m; i++) {
 		cin >> s;
 		if (s == "add") {
-			cin >> x;
-			if (mp.find(x) != mp.end()) continue;
-			else mp.insert({x, 1});
+			cin >> x; n |= (1 << x);
 		}
 		else if (s == "remove") {
-			cin >> x;
-			if (mp.find(x) == mp.end()) continue;
-			else mp.erase(x);
+			cin >> x; n &= ~(1 << x);
 		}
 		else if (s == "check") {
 			cin >> x;
-			if (mp.find(x) != mp.end()) cout << 1 << "\n";
+			if (n & (1 << x)) cout << 1 << "\n";
 			else cout << 0 << "\n";
 		}
 		else if (s == "toggle") {
-			cin >> x;
-			if (mp.find(x) != mp.end()) mp.erase(x);
-			else mp.insert({ x, 1 });
+			cin >> x; n ^= (1 << x);
 		}
-		else if (s == "all") {
-			for (int j = 1; j <= 20; j++) mp.insert({ j, 1 });
-		}
-		else if (s == "empty") mp.clear();
+		else if (s == "all") n = (1 << 21) - 1;
+		else if (s == "empty") n = 0;
 	}
-	return 0;
 }
