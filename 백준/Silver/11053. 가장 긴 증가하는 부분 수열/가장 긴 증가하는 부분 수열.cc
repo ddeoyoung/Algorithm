95c5ffd142c38d552b29c2a8b92686844620e307
Seoyoung Kim <ksysnake@hanyang.ac.kr>
2024-12-10 23:57:24 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,14 +1,12 @@
-// 2024.11.21
+// 2024.12.10
 // BOJ_11053_가장긴증가하는부분수열
 #include <bits/stdc++.h>
 using namespace std;
-
 int N, n, dp[1001], res;
 vector<int> v;
 
 int main() {
 	cin >> N;
-	
 	for (int i = 0; i < N; i++) {
 		cin >> n;
 		v.push_back(n);
@@ -21,7 +19,6 @@ int main() {
 		dp[i] = cur + 1;
 		res = max(res, dp[i]);
 	}
-
 	cout << res;
 	return 0;
 }
